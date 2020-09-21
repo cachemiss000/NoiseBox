32d00fe9ec9bb36f813624dc04d06eeaa2efaa73
Ian <ian@fantasticide.net>
2020-09-20 21:48:39 -0700

    Overhaul Oracles so they distinguish between "current" and "next" songs.
    You need a way to get the currently playing song in some instances, but "state" is a finnicky beast. As a result, this jumps through a whole buncha hoops to disentangle the concept of "next" and "current", to lay out expectations of behavior for the two that a standard human might have, and to ensure the code's logic adheres to those expectations.
    
    We also begin making some guarantees around the interface, such as "current_song" will never change it's return value until "next_song" is called. This has ripple effects, but should make it easier for potential users and integrators to wrap their heads around what's going on.

--- oracles.py
+++ oracles.py
@@ -3,15 +3,77 @@ from typing import List, Union
 
 class Oracle(object):
     """
-    A class which returns the next song to play, assuming the previous song has completed.\
+    A class which returns the next song to play, assuming the previous song has completed.
+
+    NOTE: When created, the "first song" is returned by "current_song()", while the *SECOND* song is returned
+    when you call "next_song()". As a result, you will miss things if you just start by calling "next_song()", although
+    after you collect "current_song()" the first time, you'll get the full playlist if you continue calling "next_song()
+    until you get "None".
+
+    "current_song" will always return the same value after it is called once, until you call "next_song" (on
+    properly-implemented clients)
     """
 
-    def next_song(self) -> str:
+    def next_song(self) -> Union[str, None]:
         """
         Get the next song to play.
         :return: The filename of the song to play, or None if there are no more songs to play.
         """
         pass
+
+    def current_song(self) -> Union[str, None]:
+        """
+        Get the current song playing.
+
+        This is ALWAYS the last song returned by "next_song", and will never change after calling it *until* you
+        call "next_song" again, .
+
+        This is useful for situations where you stopped and want to restart, or otherwise don't want to advance
+        the currently-playing song, but do need the filepath again.
+
+        This WILL NEVER change internal state except for memoization, which can lead to some *seriously* funky
+        consequences. For instance, if you have a chain oracle with [PlaylistOracle[None], PlaylistOracle[1, 2, 3]] and
+         call "current_song", it'll return None while "next_song" will return "1". If you create a switch oracle and
+         then switch it after calling "next_song", it'll still return the original song. And so on. Tons of edge cases
+         when messing with state =(
+
+        :return:The filename of the current song that should be playing.
+        """
+        pass
+
+
+class MemoizingOracle(Oracle):
+    """
+    An oracle which fulfills the memoizing aspects of the interface as laid out above.
+
+    We separate the implementation of this oracle because memoizing is complicated and a pain, especially when you
+    need to do it 15 times. We don't put this logic into the base Oracle class, however, because the PlaylistOracle
+    and other simple oracles don't need it.
+    """
+
+    def __init__(self):
+        self.has_memoized_current_song = False
+        self.memoized_current_song = None
+
+    def _inner_next_song(self) -> Union[str, None]:
+        raise NotImplementedError("No implementation found for __inner_next_song in '%s'" % (self.__class__,))
+
+    def _inner_get_first_song(self) -> Union[str, None]:
+        raise NotImplementedError("No implementation found for __inner_next_song in '%s'" % (self.__class__,))
+
+    def current_song(self):
+        # We make the distinction between "has_memoized_current_song" and "memoized_current_song != None"
+        # because __inner_current_song *might* return None.
+        if not self.has_memoized_current_song:
+            self.memoized_current_song = self._inner_get_first_song()
+            self.has_memoized_current_song = True
+        return self.memoized_current_song
+
+    def next_song(self):
+        next_song = self._inner_next_song()
+        self.memoized_current_song = next_song
+        self.has_memoized_current_song = True
+        return next_song
 
 
 class PlaylistOracle(Oracle):
@@ -26,14 +88,19 @@ class PlaylistOracle(Oracle):
         self.__pointer = 0
 
     def next_song(self) -> Union[str, None]:
+        self.__pointer += 1
         if self.__pointer >= len(self.__playlist):
             return None
         return_val = self.__playlist[self.__pointer]
-        self.__pointer += 1
         return return_val
 
+    def current_song(self) -> Union[str, None]:
+        if self.__pointer >= len(self.__playlist):
+            return None
+        return self.__playlist[self.__pointer]
 
-class ChainOracle(Oracle):
+
+class ChainOracle(MemoizingOracle):
     """
     An Oracle that will iterate through a list of oracles.
 
@@ -41,22 +108,66 @@ class ChainOracle(Oracle):
     """
 
     def __init__(self):
+        super(ChainOracle, self).__init__()
         self.__oracles = []
+        # Start at -1 so we know to grab the first song from the first oracle using "current_song" instead of
+        # assuming we've already started on that oracle and jumping straigt to "next_song"
         self.__pointer = 0
 
-    def next_song(self) -> Union[str, None]:
+        # We want "next_song" to return the second song if it's called before ever calling "current_song()" for
+        # reasons that are stupid and hard to explain at 1am. Play with the tests if you don't believe me.
+        #
+        # Otherwise, this variable basically means "If true, take the "current_song()" of the current oracle instead
+        # of the "next_song()" because we haven't returned the "current_song()" yet.
+        self.__has_drawn_from_oracle = True
+
+    def _inner_next_song(self) -> Union[str, None]:
         song = None
         while song is None:
-            # Short circuit the loop if there's nothing left to return.
-            if self.__pointer >= len(self.__oracles):
+            current_oracle = self.__get_current_oracle()
+            if current_oracle is None:
                 return None
-            if self.__pointer >= len(self.__oracles):
-                return None
-            oracle = self.__oracles[self.__pointer]
-            song = oracle.next_song()
+
+            # This covers some nasty edge cases, like "going off the edge in one call
+            # to "next_song()" and then adding an oracle and calling "next_song()" again.
+            if self.__has_drawn_from_oracle:
+                song = current_oracle.next_song()
+            else:
+                song = current_oracle.current_song()
+
+            # This is so ugly I hate it. Please someone help.
             if song is None:
                 self.__pointer += 1
+                self.__has_drawn_from_oracle = False
+
+        self.__has_drawn_from_oracle = True
         return song
+
+    def __get_current_oracle(self) -> Union[Oracle, None]:
+        """A quick encapsulation of the 'get next' logic to make code function more smoothly
+
+        :return the current oracle, and empty oracle if we haven't called "NextSong" yet, and
+        """
+        if self.__pointer >= len(self.__oracles):
+            return None
+
+        return self.__oracles[self.__pointer]
+
+    def _inner_get_first_song(self) -> Union[str, None]:
+        if self.__oracles is None or len(self.__oracles) == 0:
+            # Ensure that next time next_song() gets called, we force ourselves to play the first song of whatever
+            # oracles have been added since then.
+            self.__oracles.insert(0, PlaylistOracle([]))
+            return None
+
+        if self.__oracles[0].current_song() is None:
+            # Same as above - make sure we play the first song of the first oracle if it gets updated before we call
+            # next_song()
+            self.__oracles.insert(0, PlaylistOracle([]))
+            return None
+
+        self.__has_drawn_from_oracle = True
+        return self.__oracles[0].current_song()
 
     def add(self, oracle: Union[Oracle, None]):
         if oracle is None:
@@ -65,37 +176,95 @@ class ChainOracle(Oracle):
 
     def clear(self):
         self.__oracles.clear()
+        self.__has_drawn_from_oracle = False
 
 
-class SwitchOracle(Oracle):
+class SwitchOracle(MemoizingOracle):
     """
-    Allows switching between multiple oracles
+    Allows switching between multiple oracles.
+
+    This oracle has an edge case where, if you call "next_song()" after setting the first oracle but before
+    calling literally anything else, it'll return the first song of that oracle.
     """
 
     def __init__(self):
+        super(SwitchOracle, self).__init__()
         self.__oracle = None
 
-    def next_song(self) -> Union[str, None]:
+        # We need to make sure we return the first song of an oracle using "current_song()" when calling "next_song()"
+        # on the SwitchOracle whenever we switch Oracles. So if you have:
+        #    p1, p2  = (PlaylistOracle([1, 2, 3]), PlaylistOracle([4, 5, 6])
+        #    switch = SwitchOracle()
+        #    switch.set_oracle(p1)
+        # and you collect some songs from 'switch' before switching to p2, you'd want the next call to "next_song()" to
+        # return "4" instead of "5" (which is what'd be returned by calling 'next_song()' on p2)
+        self.__has_gathered_from_oracle = True
+
+        # This one's tricky. If you do something like:
+        #   p = PlaylistOracle([1, 2, 3])
+        #   o = SwitchOracle()
+        #   o.set_oracle(p)
+        #   o.next_song()
+        # You'd expect it to return "2". BUT: If you instead call:
+        #   p = PlaylistOracle(...)
+        #   o = SwitchOracle()
+        #   o.current_song()
+        #   o.set_oracle(p)
+        #   o.next_song()
+        # You'd expect it to return "1" - meaning that the "None" state acts like it's own Oracle only if it's looked
+        # at before assigning an actual Oracle to the SwitchOracle.
+        #
+        # So: This tracks whether "_get_first_song()" has been called, indicating the second situation, and alters
+        # the behavior of _inner_next_song() accordingly.
+        #
+        # We use a "proscriptive" name here rather than "descriptive" because it's such an effed situation and hopefully
+        # any modifiers will look at this comment before breaking the class (although there's a test for this behavior).
+        self.__ignore_first_song = True
+
+    def _inner_next_song(self) -> Union[str, None]:
         if self.__oracle is None:
             return None
-        return self.__oracle.next_song()
+        if self.__has_gathered_from_oracle or self.__ignore_first_song:
+            self.__ignore_first_song = False
+            return self.__oracle.next_song()
+
+        song = self.__oracle.current_song()
+        self.__has_gathered_from_oracle = True
+        return song
+
+    def _inner_get_first_song(self) -> Union[str, None]:
+        self.__ignore_first_song = False
+        if self.__oracle is None:
+            return None
+        song = self.__oracle.current_song()
+        self.__has_gathered_from_oracle = True
+        return song
 
     def set_oracle(self, oracle: Union[Oracle, None]):
         self.__oracle = oracle
+        self.__has_gathered_from_oracle = False
 
 
-class InterruptOracle(Oracle):
+class InterruptOracle(MemoizingOracle):
     """
     Allows interrupting an oracle, then going back to whatever was being done before.
     """
 
     def __init__(self, default_oracle: Union[Oracle, None]):
+        super(InterruptOracle, self).__init__()
         self.__default_oracle = default_oracle
+        self._grabbed_first_song = False
         self.__interrupt_oracle = None
 
-    def next_song(self) -> Union[str, None]:
+    def _inner_next_song(self) -> Union[str, None]:
         if self.__interrupt_oracle is not None:
-            next_song = self.__interrupt_oracle.next_song()
+            # We want to make sure to grab the first song from an oracle when it's interrupting
+            # a current oracle before moving onto the second song.
+            if not self._grabbed_first_song:
+                next_song = self.__interrupt_oracle.current_song()
+                self._grabbed_first_song = True
+            else:
+                next_song = self.__interrupt_oracle.next_song()
             if next_song is not None:
                 return next_song
 
@@ -106,8 +275,17 @@ class InterruptOracle(Oracle):
             return None
         return self.__default_oracle.next_song()
 
+    def _inner_get_first_song(self) -> Union[str, None]:
+        if self.__interrupt_oracle is not None:
+            self._grabbed_first_song = True
+            return self.__interrupt_oracle.current_song()
+        if self.__default_oracle is None:
+            return None
+        return self.__default_oracle.current_song()
+
     def interrupt(self, oracle: Union[Oracle, None]):
         self.__interrupt_oracle = oracle
+        self._grabbed_first_song = False
 
     def clear_interrupt(self):
         self.__interrupt_oracle = None
@@ -119,27 +297,34 @@ class RepeatingOracle(Oracle):
     """
 
     def __init__(self, playlist: List[str], times=None):
-        self.playlist = playlist
+        super(RepeatingOracle, self).__init__()
+        self.__playlist = playlist
         # We subtract 1 here because we go through the list once by default before resetting in "next_song".
         # If we didn't, we'd go through the list x+1 times.
         self.times = times - 1 if times is not None else None
-        self.pointer = -1
+        self.__pointer = 0
 
-    def next_song(self) -> str:
-        if self.playlist is None:
+    def next_song(self) -> Union[str, None]:
+        """Guarantees that 'pointer' is always in bounds."""
+        if self.__playlist is None:
             return None
-        self.pointer += 1
+        self.__pointer += 1
 
         # If we have repeat on forever, or we need to repeat and there are repeats left.
-        if self.times is None or (self.pointer >= len(self.playlist) and self.times > 0):
-            self.pointer = self.pointer % len(self.playlist)
+        if self.times is None or (self.__pointer >= len(self.__playlist) and self.times > 0):
+            self.__pointer = self.__pointer % len(self.__playlist)
 
             # Sanity check.
             if self.times is not None:
                 self.times -= 1
 
         # No more repetitions.
-        if self.pointer >= len(self.playlist):
+        if self.__pointer >= len(self.__playlist):
             return None
 
-        return self.playlist[self.pointer]
+        return self.__playlist[self.__pointer]
+
+    def current_song(self) -> Union[str, None]:
+        if self.__playlist is None or len(self.__playlist) == 0 or self.__pointer >= len(self.__playlist):
+            return None
+        return self.__playlist[self.__pointer]
--- oracles_test.py
+++ oracles_test.py
@@ -7,8 +7,10 @@ import oracles
 
 def collect(oracle: oracles.Oracle) -> List[str]:
     """Get up to 100 songs from the oracle. If you need more there's something wrong with your test."""
-    songs = []
-    for i in range(0, 100):
+    # Grab the starting song
+    songs = [oracle.current_song()]
+    for i in range(0, 99):
+        # Get 99 "next" songs, for a total of 100.
         song = oracle.next_song()
         if song is None:
             return songs
@@ -25,12 +27,15 @@ class PlaylistTest(unittest.TestCase):
         self.assertIsNone(o.next_song())
 
     def test_empty_list(self):
+        self.assertIsNone(oracles.PlaylistOracle(None).current_song())
         self.assertIsNone(oracles.PlaylistOracle(None).next_song())
+
+        self.assertIsNone(oracles.PlaylistOracle([]).current_song())
         self.assertIsNone(oracles.PlaylistOracle([]).next_song())
 
     def test_abuse(self):
         o = oracles.PlaylistOracle(["1"])
-        o.next_song()
+        self.assertEqual(o.current_song(), "1")
         for _ in range(0, 5000):
             self.assertIsNone(o.next_song())
 
@@ -65,7 +70,8 @@ class ChainTest(unittest.TestCase):
         collected2 = collect(o)
 
         self.assertListEqual(collected1, songs1)
-        self.assertListEqual(collected2, songs2)
+        # Chop off the memoized None at the beginning.
+        self.assertListEqual(collected2[1:], songs2)
 
     def test_add_halfway_through(self):
         songs1 = ["1", "2", "3", "4"]
@@ -75,21 +81,24 @@ class ChainTest(unittest.TestCase):
         o = oracles.ChainOracle()
 
         o.add(p1)
-        collected = [o.next_song(), o.next_song()]
+        collected = [o.current_song(), o.next_song()]
         o.add(p2)
-        collected.extend(collect(o))
+
+        # Shave the first entry off "collect" since we already got it.
+        collected.extend(collect(o)[1:])
 
         self.assertListEqual(collected, songs1 + songs2)
 
     def test_add_empties(self):
         songs = ["1", "2", "3"]
-        p1 = oracles.PlaylistOracle(songs)
         o = oracles.ChainOracle()
         o.add(None)
         o.add(oracles.PlaylistOracle([]))
-        o.add(p1)
+        o.add(oracles.PlaylistOracle(songs))
 
-        self.assertListEqual(collect(o), songs)
+        self.assertIsNone(o.current_song())
+        # Chop off the "None" from the start because of the first "current_song" call.
+        self.assertListEqual(collect(o)[1:], songs)
 
     def test_empty_start(self):
         songs = ["1", "2", "3"]
@@ -97,12 +106,16 @@ class ChainTest(unittest.TestCase):
         p2 = oracles.PlaylistOracle(songs)
 
         o = oracles.ChainOracle()
-        song0 = o.next_song()
+        song0 = o.current_song()
+        song1 = o.next_song()
         o.add(p1)
         o.add(p2)
+        collected = collect(o)
 
         self.assertIsNone(song0)
-        self.assertListEqual(collect(o), songs * 2)
+        self.assertIsNone(song1)
+        # Chop off the memoized "None" at the beginning.
+        self.assertListEqual(collected[1:], songs * 2)
 
     def test_clear(self):
         songs1 = ["1", "2", "3"]
@@ -114,7 +127,7 @@ class ChainTest(unittest.TestCase):
         # collect 2 of p2, then clear again and collect one last song.
         o = oracles.ChainOracle()
         o.add(p1)
-        collected = [o.next_song(), o.next_song()]
+        collected = [o.current_song(), o.next_song()]
         o.clear()
         o.add(p2)
         collected.extend([o.next_song(), o.next_song()])
@@ -123,9 +136,107 @@ class ChainTest(unittest.TestCase):
 
         self.assertListEqual(collected, ["1", "2", "4", "5", None])
 
+    def test_none_returning_current_song_sticks(self):
+        songs = ["1", "2", "3"]
+        p = oracles.PlaylistOracle(songs)
+        o = oracles.ChainOracle()
+
+        no_song_available_rv = o.current_song()
+        o.add(p)
+        memoized_to_none_rv = o.current_song()
+        next_song_rv = o.next_song()
+
+        self.assertIsNone(no_song_available_rv)
+        self.assertIsNone(memoized_to_none_rv)
+        self.assertEqual(next_song_rv, "1")
+
+    def test_none_after_finishing_chain_sticks(self):
+        songs = ["1", "2", "3"]
+        p1 = oracles.PlaylistOracle(songs)
+        p2 = oracles.PlaylistOracle(songs)
+        o = oracles.ChainOracle()
+
+        o.add(p1)
+        collected = collect(o)
+
+        no_songs_available_rv = o.next_song()
+        o.add(p2)
+        memoized_to_none_rv = o.current_song()
+        start_next_list_rv = o.next_song()
+
+        self.assertEqual(collected, songs)
+        self.assertIsNone(no_songs_available_rv)
+        self.assertIsNone(memoized_to_none_rv)
+        self.assertEqual(start_next_list_rv, "1")
+
+    def test_clearing_oracles_memoizes_last_song(self):
+        songs1 = ["1", "2", "3"]
+        p1 = oracles.PlaylistOracle(songs1)
+        songs2 = ["4", "5"]
+        p2 = oracles.PlaylistOracle(songs2)
+        o = oracles.ChainOracle()
+
+        o.add(p1)
+        first_song_rv = o.current_song()
+        o.clear()
+        memoized_first_song_rv = o.current_song()
+        empty_next_song_rv = o.next_song()
+        still_empty_current_song_rv = o.next_song()
+
+        o.add(p2)
+        # This should be the second song since "p" maintains its state even after being cleared.
+        second_song_rv = o.next_song()
+
+        self.assertEqual(first_song_rv, "1")
+        self.assertEqual(memoized_first_song_rv, "1")
+        self.assertIsNone(empty_next_song_rv)
+        self.assertIsNone(still_empty_current_song_rv)
+        self.assertEqual(second_song_rv, "4")
+
+    def test_immediate_next_song_returns_second_normally(self):
+        songs = ["1", "2"]
+        p = oracles.PlaylistOracle(songs)
+        o = oracles.ChainOracle()
+
+        o.add(p)
+
+        self.assertEqual(o.next_song(), "2")
+
+    def test_adding_after_memoizing_none_plays_first_song_before_adding_other_oracles(self):
+        songs = ["1"]
+        p = oracles.PlaylistOracle(songs)
+        o = oracles.ChainOracle()
+
+        memoizing_none_rv = o.current_song()
+        o.add(p)
+        still_memoized_none_rv = o.current_song()
+        first_song_of_new_oracle_rv = o.next_song()
+
+        self.assertIsNone(memoizing_none_rv)
+        self.assertIsNone(still_memoized_none_rv)
+        self.assertEqual(first_song_of_new_oracle_rv, "1")
+
+    def test_adding_after_memoizing_none_plays_first_song_after_adding_other_oracles(self):
+        songs1 = ["1"]
+        p1 = oracles.PlaylistOracle(songs1)
+        songs2 = ["2"]
+        p2 = oracles.PlaylistOracle(songs2)
+        o = oracles.ChainOracle()
+
+        o.add(p1)
+        first_song_rv = o.current_song()
+        off_the_end_rv = o.next_song()
+        o.add(p2)
+        memoized_off_the_end_rv = o.current_song()
+        second_song_rv = o.next_song()
+
+        self.assertEqual(first_song_rv, "1")
+        self.assertIsNone(off_the_end_rv)
+        self.assertIsNone(memoized_off_the_end_rv)
+        self.assertEqual(second_song_rv, "2")
 
 
-class TestSwitchOracle(unittest.TestCase):
+class SwitchOracleTest(unittest.TestCase):
 
     def test_no_oracle(self):
         o = oracles.SwitchOracle()
@@ -136,10 +247,13 @@ class TestSwitchOracle(unittest.TestCase):
         o = oracles.SwitchOracle()
         p1 = oracles.PlaylistOracle(songs)
 
-        o.next_song()
+        no_song_available_rv = o.current_song()
         o.set_oracle(p1)
 
-        self.assertListEqual(collect(o), songs)
+        self.assertIsNone(no_song_available_rv)
+        # Collect will return "None" first because we've memoized
+        # "None" above, and collect calls "current_song()" first.
+        self.assertListEqual(collect(o), [None] + songs)
 
     def test_set_to_none(self):
         songs = ["1", "2", "3"]
@@ -148,7 +262,7 @@ class TestSwitchOracle(unittest.TestCase):
 
         # Only get 2 songs so that we can make sure the third *isn't* collected when we switch.
         o.set_oracle(p1)
-        collected = [o.next_song(), o.next_song()]
+        collected = [o.current_song(), o.next_song()]
         o.set_oracle(None)
 
         self.assertIsNone(o.next_song())
@@ -164,13 +278,53 @@ class TestSwitchOracle(unittest.TestCase):
         p3 = oracles.PlaylistOracle(songs3)
 
         o.set_oracle(p1)
-        collected = [o.next_song(), o.next_song()]
+        collected = [o.current_song(), o.next_song()]
         o.set_oracle(p2)
         collected.extend([o.next_song(), o.next_song(), o.next_song(), o.next_song()])
         o.set_oracle(p3)
         collected.extend([o.next_song(), o.next_song(), o.next_song()])
 
         self.assertListEqual(collected, list(itertools.chain(songs1[:2], songs2, [None], songs3)))
+
+    def test_memoize_to_none_and_play_first_with_current_song_before_setting(self):
+        songs = ["1", "2"]
+        p = oracles.PlaylistOracle(songs)
+        o = oracles.SwitchOracle()
+
+        no_songs_set_rv = o.current_song()
+        o.set_oracle(p)
+        memoized_to_none_rv = o.current_song()
+        first_song_of_playlist_rv = o.next_song()
+
+        self.assertIsNone(no_songs_set_rv)
+        self.assertIsNone(memoized_to_none_rv)
+        self.assertEqual(first_song_of_playlist_rv, "1")
+
+    def test_go_to_second_song_without_memoizing_call_to_current_song(self):
+        songs = ["1", "2"]
+        p = oracles.PlaylistOracle(songs)
+        o = oracles.SwitchOracle()
+
+        o.set_oracle(p)
+
+        self.assertEqual(o.next_song(), "2")
+
+    def test_memoize_old_oracles_after_switching(self):
+        songs1 = ["1"]
+        p1 = oracles.PlaylistOracle(songs1)
+        songs2 = ["2"]
+        p2 = oracles.PlaylistOracle(songs2)
+        o = oracles.SwitchOracle()
+
+        o.set_oracle(p1)
+        first_song_on_p1_rv = o.current_song()
+        o.set_oracle(p2)
+        memoized_first_song_from_p1_rv = o.current_song()
+        new_first_song_from_p2_rv = o.next_song()
+
+        self.assertEqual(first_song_on_p1_rv, "1")
+        self.assertEqual(memoized_first_song_from_p1_rv, "1")
+        self.assertEqual(new_first_song_from_p2_rv, "2")
 
 
 class InterruptOracleTest(unittest.TestCase):
@@ -189,11 +343,11 @@ class InterruptOracleTest(unittest.TestCase):
         p2 = oracles.PlaylistOracle(songs2)
         o = oracles.InterruptOracle(p1)
 
-        collected = [o.next_song(), o.next_song()]
+        collected = [o.current_song(), o.next_song()]
         o.interrupt(p2)
         collected.extend(collect(o))
 
-        self.assertListEqual(collected, ["1", "2", "4", "5", "6", "3"])
+        self.assertListEqual(collected, ["1", "2", "2", "4", "5", "6", "3"])
 
     def test_interrupt_after_finish(self):
         songs1 = ["1", "2", "3"]
@@ -213,13 +367,24 @@ class InterruptOracleTest(unittest.TestCase):
         p1 = oracles.PlaylistOracle(songs)
         o = oracles.InterruptOracle(None)
 
-        collected = [o.next_song()]
+        collected = [o.current_song()]
         o.interrupt(p1)
         collected.extend(collect(o))
 
-        expected = list([None])
+        # Two "None"s, one from the initial "current_song", one from the "current_song" in collect()
+        expected = list([None, None])
         expected.extend(songs)
         self.assertListEqual(collected, expected)
+
+    def test_no_default_immediately_start(self):
+        songs = ["1", "2", "3"]
+        p1 = oracles.PlaylistOracle(songs)
+        o = oracles.InterruptOracle(None)
+
+        o.interrupt(p1)
+        collected = collect(o)
+
+        self.assertListEqual(collected, songs)
 
 
 class RepeatingOracleTest(unittest.TestCase):
@@ -232,6 +397,7 @@ class RepeatingOracleTest(unittest.TestCase):
         collected = collect(o)
 
         self.assertListEqual(collected, songs * 20)
+        self.assertEqual(collected[-1], o.current_song())
         self.assertIsNotNone(o.next_song())
 
     def test_5_repetitions(self):
@@ -246,8 +412,31 @@ class RepeatingOracleTest(unittest.TestCase):
         o1 = oracles.RepeatingOracle(None)
         o2 = oracles.RepeatingOracle(None, 52)
 
-        self.assertIsNone(o1.next_song())
+        self.assertIsNone(o1.current_song())
         self.assertIsNone(o2.next_song())
+
+    def test_current_song_with_repeating(self):
+        songs = ["1", "2", "3"]
+        o = oracles.RepeatingOracle(songs, 2)
+
+        collected = [o.current_song(), o.current_song()]
+        # "Current song" goes from 1->2, 2->3, 3->1, 1->2.
+        for i in range(4):
+            o.next_song()
+        collected.extend([o.current_song(), o.current_song()])
+
+        # "Current song" goes from 2->3, 3->None, and then repeats.
+        for i in range(4):
+            o.next_song()
+        collected.extend([o.current_song(), o.current_song()])
+
+        self.assertListEqual(collected, ["1", "1", "2", "2", None, None])
+
+    def test_immediate_next_song_returns_second_song(self):
+        songs = ["1", "2", "3"]
+        o = oracles.RepeatingOracle(songs)
+
+        self.assertEqual(o.next_song(), "2")
 
 
 if __name__ == '__main__':
