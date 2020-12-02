df98166009ff104c592a04834a5d05764cd6c939
Ian <ian@fantasticide.net>
2020-12-02 15:28:16 -0800

    Start implementing a V1 server. Currently only accepts toggle_play and is not hooked up to anything.
    Appologies for the mass of commented-out code. I'm switching over to development of a muxer, and I want to keep that code around. It should be gone by the next pull request.

--- commandserver/v1_server.py
+++ commandserver/v1_server.py
@@ -1,13 +1,16 @@
 import collections
-import websockets
-import json
-from typing import List, Tuple
+from enum import Enum
+
+from typing import List, Tuple, Optional
 
 import commandserver.command_types_v1 as c_types
+from commandserver import flags as server_flags
 from medialogic import media_library, controller
+from common import flags
 
-
-DEFAULT_MAX_RESPONSE_SIZE = 200
+VERSION = "V1"
+FLAGS = flags.FLAGS
+FLAGS.register_flags(server_flags.flags)
 
 # TODO: For type sanity, this NEEDS to be a class. Goddamn.
 Page = collections.namedtuple("Page", ["list_hash", "element"])
@@ -17,13 +20,38 @@ class OutdatedPageException(Exception):
     pass
 
 
+class ErrorDataType(Enum):
+    """Dictates what kind of error data is attached to an exception.
+
+    Used for scrubbing debug data when debug is false.
+    """
+
+    # Running out in the wild. Scrub debug data.
+    PRODUCTION: int = 0
+
+    # Running in dev mode. Keep debug data.
+    DEBUG: int = 1
+
+
 class ErrorResponse(Exception, c_types.Response):
-    pass
+
+    def __init__(self, error_data_type=ErrorDataType.DEBUG, **kwargs):
+        """Really gross manual initialization of base classes.
+
+         This ensures the Exception class gets the error message.
+
+         Also removes error_data if it contains Debug data.
+         """
+        if error_data_type == ErrorDataType.DEBUG and not FLAGS.debug:
+            kwargs.pop("error_data", None)
+
+        Exception.__init__(self, kwargs.get("error_message"))
+        c_types.Response.__init__(self, **kwargs)
 
 
 # Command names in this list do not need a parameter list to execute successfully, and will
 # not be checked for them by the command validation code.
-PARAMETERLESS_COMMANDS = [
+PARAMETERLESS_COMMANDS: List[str] = [
     c_types.TogglePlayCommand.COMMAND_NAME,
     c_types.NextSongCommand.COMMAND_NAME,
     c_types.ListSongsCommand.COMMAND_NAME,
@@ -32,69 +60,107 @@ PARAMETERLESS_COMMANDS = [
 
 
 class MediaServer:
-    def __init__(self, controller: controller.Controller, media_library: media_library.MediaLibrary):
+    def __init__(self, c: controller.Controller, ml: media_library.MediaLibrary):
         super(MediaServer, self).__init__()
-        self.media_library = media_library
-        self.controller = controller
+        self.media_library = ml
+        self.controller = c
 
     @classmethod
     def parse_command(cls, command: str) -> c_types.Command:
         """Parses a JSON command from the input string as expected over a raw websocket."""
-        json_result = c_types.Command.from_json(command)
-        if json_result.command_name is None:
-            raise ErrorResponse(command_status=c_types.ResponseState.CLIENT_ERROR,
+        # infer_missing returns missing fields as None, which is always the desired behavior
+        # in our case. Otherwise, empty optional fields would throw.
+        command_result = c_types.Command.from_json(command, infer_missing=True)
+        if command_result.command_name is None:
+            raise ErrorResponse(command_status=c_types.CommandStatus.CLIENT_ERROR,
                                 error_message="Command name must be specified on the input command",
+                                error_data=command,
+                                error_data_type=ErrorDataType.DEBUG)
+
+        if command_result.command_name not in c_types.COMMAND_NAMES:
+            raise ErrorResponse(command_status=c_types.CommandStatus.CLIENT_ERROR,
+                                error_message="Command name '%s' is not a valid %s command" % (
+                                    command_result.command_name, VERSION),
+                                error_data=command,
+                                error_data_type=ErrorDataType.DEBUG)
+
+        if command_result.command_name in PARAMETERLESS_COMMANDS:
+            return command_result  # No error checking needed if it otherwise parsed correctly.
+
+        if command_result.payload is None:
+            raise ErrorResponse(command_status=c_types.CommandStatus.CLIENT_ERROR,
+                                error_message="Payload expected for command '%s'" % (command_result.command_name,),
                                 error_data=command)
+        return command_result
 
-        if json_result.command_name in PARAMETERLESS_COMMANDS:
-            return  # No error checking needed if it otherwise parsed correctly.
+    def accept(self, command_str: str) -> c_types.Response:
+        try:
+            command = MediaServer.parse_command(command_str)
+            if command.command_name == c_types.TogglePlayCommand.COMMAND_NAME:
+                return self.toggle_play(
+                    c_types.TogglePlayCommand.from_dict(command.payload) if command.payload else None)
+        except ErrorResponse as e:
+            return e
+        except Exception as e:
+            return ErrorResponse(
+                command_status=c_types.CommandStatus.INTERNAL_ERROR,
+                error_message="Unexpected error encountered: '%s'" % (e,),
+                error_data=e)
 
-        if json_result.payload is None:
-            raise ErrorResponse(command_status=c_types.ResponseState.CLIENT_ERROR,
-                                error_message="Payload expected for command '%s'" % (json_result.command_name,),
-                                error_data=command)
+    def toggle_play(self, play_request: Optional[c_types.TogglePlayCommand]) -> c_types.Response:
+        """Toggles the play/pause state, with "play" == True.
 
+        This contrasts with VLC's interpretation, where "pause" == True, which is standard further down
+        the stack.
+        """
+        if play_request is None or play_request.play_state is None:
+            self.controller.toggle_pause()
+            return c_types.TogglePlayResponse(new_play_state=self.controller.playing(),
+                                              command_status=c_types.CommandStatus.SUCCESS)
 
+        # Invert play_state because we're setting the pause value.
+        # Vlc has some weird semantics. I refuse to build around them at this level.
+        self.controller.set_pause(not play_request.play_state)
 
+        return c_types.TogglePlayResponse(new_play_state=self.controller.playing(),
+                                          command_status=c_types.CommandStatus.SUCCESS)
 
-    def Play(self, play_request, context) -> ms_pb.PlayResponse:
-        self.controller.resume()
-        return ms_pb.PlayResponse()
-
-    def ListSongs(self, list_songs_request: ms_pb.ListSongsRequest, ctx) -> ms_pb.ListSongsResponse:
-        songs = self.media_library.list_songs()
-
-        if list_songs_request.max_num_entries:
-            max_response_size = list_songs_request.max_num_entries
-        else:
-            max_response_size = DEFAULT_MAX_RESPONSE_SIZE
-        next_page, next_page_token = get_output_page(songs, max_response_size,
-                                                     string_to_page(list_songs_request.page_token))
-        response = ms_pb.ListSongResponse()
-        response.song_names.extend(next_page)
-        response.next_page_token = page_to_string(next_page_token)
-        return response
-
-    def add_to_server(self, server: grpc.Server):
-        ms_pb_grpc.add_MediaControlServiceServicer_to_server(self, server)
-
-    def ListPlaylists(self, list_playlists_request: ms_pb.ListPlaylistsRequest, ctx) -> ms_pb.ListPlaylistsResponse:
-        playlist_tuples = self.media_library.list_playlists()
-        # Playlist tuples have two entries: The playlist name, then a list of all songs in the playlist.
-        # TODO: Playlists should be a class, just like songs.
-        playlists = (pt[0] for pt in playlist_tuples)
-
-        if list_playlists_request.max_num_entries:
-            max_response_size = list_playlists_request.max_num_entries
-        else:
-            max_response_size = DEFAULT_MAX_RESPONSE_SIZE
-
-        next_page, next_page_token = get_output_page(list(playlists), max_response_size,
-                                                     string_to_page(list_playlists_request.page_token))
-        response = ms_pb.ListPlaylistsResponse()
-        response.playlist_names.extend(next_page)
-        response.next_page_token = page_to_string(next_page_token)
-        return response
+    #
+    # def ListSongs(self, list_songs_request: ms_pb.ListSongsRequest, ctx) -> ms_pb.ListSongsResponse:
+    #     songs = self.media_library.list_songs()
+    #
+    #     if list_songs_request.max_num_entries:
+    #         max_response_size = list_songs_request.max_num_entries
+    #     else:
+    #         max_response_size = DEFAULT_MAX_RESPONSE_SIZE
+    #     next_page, next_page_token = get_output_page(songs, max_response_size,
+    #                                                  string_to_page(list_songs_request.page_token))
+    #     response = ms_pb.ListSongResponse()
+    #     response.song_names.extend(next_page)
+    #     response.next_page_token = page_to_string(next_page_token)
+    #     return response
+    #
+    # def add_to_server(self, server: grpc.Server):
+    #     ms_pb_grpc.add_MediaControlServiceServicer_to_server(self, server)
+    #
+    # def ListPlaylists(self, list_playlists_request: ms_pb.ListPlaylistsRequest, ctx) -> ms_pb.ListPlaylistsResponse:
+    #     playlist_tuples = self.media_library.list_playlists()
+    #     # Playlist tuples have two entries: The playlist name, then a list of all songs in the playlist.
+    #     # TODO: Playlists should be a class, just like songs.
+    #     playlists = (pt[0] for pt in playlist_tuples)
+    #
+    #     if list_playlists_request.max_num_entries:
+    #         max_response_size = list_playlists_request.max_num_entries
+    #     else:
+    #         max_response_size = DEFAULT_MAX_RESPONSE_SIZE
+    #
+    #     next_page, next_page_token = get_output_page(list(playlists), max_response_size,
+    #                                                  string_to_page(list_playlists_request.page_token))
+    #     response = ms_pb.ListPlaylistsResponse()
+    #     response.playlist_names.extend(next_page)
+    #     response.next_page_token = page_to_string(next_page_token)
+    #     return response
+    #
 
 
 def get_output_page(sequence: List[object], max_length: int, next_page_token: Page) -> Tuple[List[object], Page]:
--- testing/commandserver/v1_server_test.py
+++ testing/commandserver/v1_server_test.py
@@ -1,29 +1,210 @@
 import json
 import unittest
+from unittest.mock import Mock
 
+from common import flags
 from parameterized import parameterized
 from commandserver import command_types_v1 as c_types
-from commandserver.v1_server import MediaServer
+from commandserver import v1_server as server
+from medialogic import controller, media_library
+
+# We can do this because it's a test file, so we know it's top-level :P
+FLAGS = flags.FLAGS
+FLAGS.init()
+FLAGS.override_flag("debug", True)
+
+
+class MockController(Mock):
+    def __init__(self):
+        super(MockController, self).__init__(spec=controller.Controller)
+        self.playing_state = False
+
+    def set_pause(self, pause_state):
+        self.playing_state = not pause_state
+        return not self.playing_state
+
+    def toggle_pause(self):
+        self.playing_state = not self.playing_state
+
+    def playing(self):
+        return self.playing_state
+
+    def paused(self):
+        return not self.playing_state
+
+
+def mock_server() -> (server.MediaServer, controller.Controller, media_library.MediaLibrary):
+    c = MockController()
+    ml = Mock()
+    media_server = server.MediaServer(c, ml)
+
+    return media_server, c, ml
 
 
 class CommandParsingTest(unittest.TestCase):
 
     def test_parameterless_not_empty(self):
         """Sanity test to ensure the next test isn't running on the empty set."""
-        self.assertGreater(len(c_types.PARAMETERLESS_COMMANDS), 1,
-                           "Parameterless commands '%s' should not be empty" % (c_types.PARAMETERLESS_COMMANDS,))
+        self.assertGreater(len(server.PARAMETERLESS_COMMANDS), 1,
+                           "Parameterless commands '%s' should not be empty" % (server.PARAMETERLESS_COMMANDS,))
 
-    @parameterized.expand(c_types.PARAMETERLESS_COMMANDS)
-    def test_parse_empty_commands(self, command_cls: c_types.CommandCls):
+    @parameterized.expand(server.PARAMETERLESS_COMMANDS)
+    def test_parse_empty_commands(self, command_cls: str):
         command = {
-            "command_name": command_cls.COMMAND_NAME
+            "command_name": command_cls
         }
 
-        output_command = MediaServer.parse_command(json.dumps(command))
+        output_command = server.MediaServer.parse_command(json.dumps(command))
 
-        self.assertTrue(isinstance((output_command, c_types.Command)))
-        self.assertEqual(output_command.command_name, command_cls.COMMAND_NAME)
+        self.assertTrue(isinstance(output_command, c_types.Command))
+        self.assertEqual(output_command.command_name, command_cls)
         self.assertIsNone(output_command.payload)
+
+    def test_throws_no_command_set(self):
+        command = {
+            "payload": {
+                "play_state": True
+            }
+        }
+
+        with self.assertRaises(server.ErrorResponse) as ex:
+            server.MediaServer.parse_command(json.dumps(command))
+        exception = ex.exception
+        self.assertRegex(exception.error_message, "Command name must be specified.*")
+        self.assertEqual(exception.command_status, c_types.CommandStatus.CLIENT_ERROR)
+
+    def test_command_not_defined(self):
+        command = {
+            "command_name": "florbus",
+            "payload": {}
+        }
+
+        with self.assertRaises(server.ErrorResponse) as ex:
+            server.MediaServer.parse_command(json.dumps(command))
+        exception = ex.exception
+        self.assertRegex(exception.error_message,
+                         "Command name 'florbus' is not a valid %s command" % (server.VERSION,))
+        self.assertEqual(exception.command_status, c_types.CommandStatus.CLIENT_ERROR)
+
+
+def is_successful_response(response: c_types.Response):
+    if response.command_status in c_types.CommandStatus.SUCCESS_TYPES:
+        return True
+
+    if isinstance(response.error_data, Exception):
+        raise AssertionError(
+            "Received error response from server: '%s'" % (response.error_message,)) from response.error_data
+
+    else:
+        raise AssertionError("Received error response from server: '%s'" % (response.error_message,))
+
+
+class PlayCommandTest(unittest.TestCase):
+
+    @parameterized.expand([(True,), (False,)])
+    def test_play_no_arg_toggles(self, starting_play_state):
+        """Check to make sure playing_state is toggled."""
+        command = {"command_name": c_types.TogglePlayCommand.COMMAND_NAME}
+        server, c, ml = mock_server()
+        c.playing_state = starting_play_state
+        response = server.accept(json.dumps(command))
+        self.assertTrue(is_successful_response(response))
+        self.assertIsNone(response.error_message)
+        self.assertIsNone(response.error_data)
+
+        # We use "assertEqual(..., not <expression)" instead of "assertNotEqual(....)"
+        # because != will return true when comparing an object to a boolean, but
+        # <object> == not <boolean> will always return false (among other reasons).
+        #
+        # IoW: This ensures we don't miss bad return types.
+        self.assertEqual(response.new_play_state, not starting_play_state)
+        self.assertEqual(c.playing(), not starting_play_state)
+
+    @parameterized.expand([(True,), (False,)])
+    def test_playing_true_false_works(self, new_play_state):
+        """Check to make sure play_state is respected."""
+        command = {
+            "command_name": c_types.TogglePlayCommand.COMMAND_NAME,
+            "payload": {
+                "play_state": new_play_state
+            }
+        }
+        server, c, ml = mock_server()
+        c.playing_state = not new_play_state
+        response = server.accept(json.dumps(command))
+        self.assertTrue(is_successful_response(response))
+        self.assertIsNone(response.error_message)
+        self.assertIsNone(response.error_data)
+        self.assertEqual(response.new_play_state, new_play_state)
+        self.assertEqual(c.playing(), new_play_state)
+
+    @parameterized.expand([(True,), (False,)])
+    def test_set_playing_idempotent(self, new_play_state):
+        """Checks to make sure the server actually sets the playing state when called."""
+        command = {
+            "command_name": c_types.TogglePlayCommand.COMMAND_NAME,
+            "payload": {
+                "play_state": new_play_state,
+            }
+        }
+        server, c, ml = mock_server()
+        c.playing_state = new_play_state
+        response = server.accept(json.dumps(command))
+        self.assertTrue(is_successful_response(response))
+        self.assertEqual(response.new_play_state, new_play_state)
+        self.assertEqual(c.playing(), new_play_state)
+
+    # Define a quick, hacky enum to make parameterized code more readable.
+    # Use strings instead of the traditional ints to make parameterized test names easier to read.
+    USE_TOGGLE = "UseToggle"
+    USE_EXPLICIT_SET = "UseExplicitSet"
+
+    @parameterized.expand([(USE_TOGGLE,), (USE_EXPLICIT_SET,)])
+    def test_setting_play_thorws_exception(self, command_type):
+        command = {"command_name": c_types.TogglePlayCommand.COMMAND_NAME}
+
+        if command_type == PlayCommandTest.USE_TOGGLE:
+            command["payload"] = {"play_state": True}
+        else:
+            assert command_type == PlayCommandTest.USE_EXPLICIT_SET
+
+        server, c, ml = mock_server()
+        ex = Exception("TestyMcTestface")
+
+        def throw_ex(unused_self=None, arg=None):
+            raise ex
+
+        c.set_pause = c.toggle_pause = throw_ex
+
+        response = server.accept(json.dumps(command))
+        self.assertRegex(response.error_message, "Unexpected error.*")
+        self.assertEqual(response.error_data, ex)
+
+    @parameterized.expand([(USE_TOGGLE,), (USE_EXPLICIT_SET,)])
+    def test_exception_data_thrown_on_debug(self, command_type):
+        """Test to ensure debug=False flag causes no internal error data to get attached to the response.
+
+        Uses FLAGS.override_flag. By default, "debug" is set to true (see top of this file).
+        """
+        command = {"command_name": c_types.TogglePlayCommand.COMMAND_NAME}
+
+        if command_type == PlayCommandTest.USE_TOGGLE:
+            command["payload"] = {"play_state": True}
+        else:
+            assert command_type == PlayCommandTest.USE_EXPLICIT_SET
+
+        server, c, ml = mock_server()
+
+        def throw_ex(unused_self, arg=None):
+            raise Exception("TestyMcTestface")
+
+        with FLAGS.override_flag("debug", False):
+            c.set_pause = c.toggle_pause = throw_ex
+            response = server.accept(json.dumps(command))
+
+        self.assertRegex(response.error_message, "Unexpected error.*")
+        self.assertEqual(response.error_data, None)
+
 
 if __name__ == '__main__':
     unittest.main()
