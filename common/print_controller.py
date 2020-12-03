7cc2e5ae5d54f76dd3f32a4f8fee45bf53562f9d
Ian <ian@fantasticide.net>
2020-12-02 17:14:12 -0800

    create a print controller that dups to logs for a given module.
    It will both print out to the file log, and, for messages over the given log level, print out to the global printer. This will help server modules express errors on bootup to the command line.
    
    Usage:
     logger = logging_printer('test_module', <optional arguments>)
     logger.info(<just like a normal log>)

--- common/print_controller.py
+++ common/print_controller.py
@@ -5,6 +5,7 @@ In particular, it implements a SystemPrintController and a MetaPrintController
 so that we can log to a file while also recording a commandline session to a file (for instance)
 with minimal disruption to the actual flow of programming code.
 """
+import logging
 
 
 class PrintController(object):
@@ -15,12 +16,14 @@ class PrintController(object):
 
 class SystemPrintController(PrintController):
     """Prints messages to stdout."""
+
     def print(self, message, *args):
         print(message, *args)
 
 
 class MetaPrintController(PrintController):
     """Prints to multiple PrintController objects whenever print() is called."""
+
     def __init__(self):
         self.controllers = []
 
@@ -35,6 +38,45 @@ class MetaPrintController(PrintController):
 
 PRINT_CONTROLLER = MetaPrintController()
 PRINT_CONTROLLER.add_print_controller(SystemPrintController())
+
+
+class PrintingLogger(PrintController):
+    def __init__(self, module_name, min_error_level_to_print: int = logging.WARNING,
+                 passthrough_printer: PrintController = PRINT_CONTROLLER):
+        self.module_name = module_name
+        self.logger = logging.getLogger(self.module_name)
+        self.min_error_level_to_print = min_error_level_to_print
+        self.passthrough_printer = passthrough_printer
+
+    def log(self, error_level: int, message, *argv):
+        if error_level >= self.min_error_level_to_print:
+            self.passthrough_printer.print(
+                "%s - %s: %s" % (self.module_name, logging.getLevelName(error_level), message), *argv)
+        self.logger.log(error_level, message, *argv)
+
+    def debug(self, message: str, *argv):
+        self.log(logging.DEBUG, message, *argv)
+
+    def info(self, message: str, *argv):
+        self.log(logging.INFO, message, *argv)
+
+    def warn(self, message: str, *argv):
+        self.log(logging.WARN, message, *argv)
+
+    def error(self, message: str, *argv):
+        self.log(logging.ERROR, message, *argv)
+
+    def critical(self, message: str, *argv):
+        self.log(logging.CRITICAL, message, *argv)
+
+
+class _GlobalPassthroughController(PrintController):
+    def print(self, msg: str, *argv):
+        print_msg(msg, *argv)
+
+
+def logging_printer(module_name: str, min_error_level_to_print: int = logging.WARNING):
+    return PrintingLogger(module_name, min_error_level_to_print, _GlobalPassthroughController())
 
 
 def add_print_controller(print_controller: PrintController):
--- testing/common/console_test.py
+++ testing/common/console_test.py
@@ -1,9 +1,15 @@
 """Tests for console.py"""
-
+import logging
 import unittest
+from dataclasses import dataclass
+from typing import List
+from unittest import mock
 
+import common
+from common import print_controller
 from localcli.console import Command
 from localcli.console import Console
+from contextlib import contextmanager
 
 
 class ConsoleTest(unittest.TestCase):
@@ -44,6 +50,96 @@ class ConsoleTest(unittest.TestCase):
         commands = list(c_out.commands(timeout=1.5))
 
         self.assertListEqual(commands, [])
+
+
+class PrinterTest(unittest.TestCase):
+
+    def test_logging_printer(self):
+        with (PrinterTestFixtures()) as test_fixtures:
+            logger = print_controller.logging_printer("foo", logging.WARN)
+
+            logger.log(logging.DEBUG, "1", "234")
+            logger.log(logging.WARN, "2", "345", "6")
+            logger.log(logging.CRITICAL, "3", "456", "7")
+
+        test_fixtures.mock_logger.log.assert_has_calls([
+            mock.call(logging.DEBUG, "1", "234"),
+            mock.call(logging.WARN, "2", "345", "6"),
+            mock.call(logging.CRITICAL, "3", "456", "7")
+        ])
+
+        expected_printer_calls = [
+            ("foo - WARNING: 2", "345", "6"),
+            ("foo - CRITICAL: 3", "456", "7")]
+        self.assertListEqual(test_fixtures.test_printer.messages, expected_printer_calls)
+
+    def test_log_level_functions(self):
+        with (PrinterTestFixtures()) as test_fixtures:
+            logger = print_controller.logging_printer("foo", logging.WARN)
+
+            logger.debug("test0")
+            logger.info("test1")
+            logger.warn("test2")
+            logger.error("test3")
+            logger.critical("test4")
+
+        test_fixtures.mock_logger.log.assert_has_calls([
+            mock.call(logging.DEBUG, "test0"),
+            mock.call(logging.INFO, "test1"),
+            mock.call(logging.WARN, "test2"),
+            mock.call(logging.ERROR, "test3"),
+            mock.call(logging.CRITICAL, "test4"),
+        ])
+
+    def test_log_random_number_for_error_level(self):
+        with (PrinterTestFixtures()) as test_fixtures:
+            logger = print_controller.logging_printer("foo", logging.WARN)
+            logger.log(500, "oh boy things are bad buddy =(")
+
+        test_fixtures.mock_logger.log.assert_called_once_with(500, "oh boy things are bad buddy =(")
+        self.assertListEqual(
+            test_fixtures.test_printer.messages,
+            [("foo - %s: oh boy things are bad buddy =(" % (logging.getLevelName(500),),)])
+
+
+class TestPrintController(print_controller.PrintController):
+    def __init__(self):
+        self.messages: List[any] = []
+
+    def print(self, msg: str, *argv):
+        self.messages.append((msg, *argv))
+
+
+class PrinterTestFixtures:
+    """Super hacky class that replaces everything we need for Printer tests with fakes and mocks.
+
+    Use a "with(TextFixtures()) as tf:" block to encapsulate ALL printer test code (arrange & act), then
+    retrieve the fakes and mocks when in the "assert" block (see CONTRIBUTIONS_README for 'arrange, act, assert'
+    explanation)
+    """
+    mock_logger: mock.Mock
+    fake_print_controller: print_controller.PrintController
+
+    def __init__(self):
+        self.mock_logger = mock.Mock()
+        self.test_printer = TestPrintController()
+
+    def _fake_get_logger(self, module_name):
+        self.mock_logger.module = module_name
+        return self.mock_logger
+
+    def __enter__(self):
+        # A massive, ugly hack because mock.patch does not like actually replacing this function =(
+        self._original_get_log_fn = print_controller.logging.getLogger
+        print_controller.logging.getLogger = self._fake_get_logger
+
+        self._original_global_printer = print_controller.PRINT_CONTROLLER
+        print_controller.PRINT_CONTROLLER = self.test_printer
+        return self
+
+    def __exit__(self, *argv, **kwargs):
+        print_controller.logging.getLogger = self._original_get_log_fn
+        print_controller.PRINT_CONTROLLER = self._original_global_printer
 
 
 if __name__ == '__main__':
