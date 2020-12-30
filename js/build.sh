a88de30f88ad8480d72032f6ea561dbf0ffa34c9
Ian <ian@fantasticide.net>
2020-12-30 13:12:32 -0800

    Fix the build system again. Duct tape continues to strain.

--- js/build.sh
+++ js/build.sh
@@ -4,5 +4,6 @@ function try() {
   "$@" || { echo "COMMAND: '$*' failed" ; exit 1; }
 }
 
+try tsc --build convertSchemaTsconfig.json
 try node dist/schema/convertToTypescript.js --out=src/schema
 try tsc --build
