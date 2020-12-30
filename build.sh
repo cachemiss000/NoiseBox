8dabdfbc99122cd8d637924a23dd00bed16b0f04
Ian <ian@fantasticide.net>
2020-12-30 12:06:29 -0800

    Some quick build-system fixes. These files probably won't last long anyway

--- build.sh
+++ build.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
 function try() {
-  $* || { echo "COMMAND: '$*' failed" ; exit 1; }
+  "$@" || { echo "COMMAND: '$*' failed" ; exit 1; }
 }
 
+# TODO: Come up with a better venv discovery method.
+source venv/Scripts/activate
+
 try python "./run.py" buildschema --out=./schema
-cd js
+try cd js
 try ./build.sh
--- js/build.sh
+++ js/build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 function try() {
-  $* || { echo "COMMAND: '$*' failed" ; exit 1; }
+  "$@" || { echo "COMMAND: '$*' failed" ; exit 1; }
 }
 
-try tsc --build
 try node dist/schema/convertToTypescript.js --out=src/schema
+try tsc --build
